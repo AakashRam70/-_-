1fd0799df2f55e963a1caf2621a7e9af76ca2eac
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-11-27 17:40:30 +0530

    modifying

--- Calculator/script.js
+++ Calculator/script.js
@@ -15,7 +15,7 @@ function deleteLast() {
 function calculate() {
     const display = document.getElementById('display');
     try {
-        display.value = eval(display.value); // Use with caution in real-world applications
+        display.value = eval(display.value);
     } catch (error) {
         alert('Invalid expression');
     }
